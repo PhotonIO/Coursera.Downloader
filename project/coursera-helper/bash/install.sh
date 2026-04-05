f5df7764958f2fb4361a91c89628ee2b8d8a27b8
Pegasus <ben@OSINT.UniSocial.net>
2026-04-05 22:25:37 +0100

    install

--- project/coursera-helper/bash/install.sh
+++ project/coursera-helper/bash/install.sh
@@ -1,16 +1,41 @@
 name=coursera-helper
-venv=~/photon/$name
+photon=~/photon
+venv=$photon/$name
+
 
 bin=$venv/bin/$name
 
-mkdir -p $venv
-cd $venv
-if [ ! -d $venv/bin ];then
-        python3 -m venv $venv
-        echo "Making virtual environment"
-fi
-if [ ! -f $bin ];then
-        $venv/bin/pip install $name
-        echo "Installed $name"
-fi
-bash
+install(){
+	if [ ! -f $bin ];then
+		mkdir -p $venv
+		cd $venv
+		if [ ! -d $venv/bin ];then
+        		python3 -m venv $venv
+        		echo "Making virtual environment"
+		fi
+
+		if [ ! -f $bin ];then
+        		$venv/bin/pip install $name
+        		echo "Installed $name"
+		fi
+	fi
+}
+
+backup(){
+	bacDir=/mnt/chromeos/MyFiles/Downloads
+	bacName=$name.clean.tgz
+	backup=$bacDir/$bacName
+
+	if [ ! -f $backup ];then
+		cd $photon
+		tar cfvz $bacName $name
+		cp -r $bacName $bacDir/
+	fi
+}
+
+main(){
+	backup
+	bash
+}
+
+main
